3a200cc2649e8f665af4797d4b8122cc3853ba1a
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-07-16 17:40:54 +0600

    Create Some Important Apis. Such as All News Api, Specific News Api and Category Api

--- index.js
+++ index.js
@@ -6,16 +6,48 @@ const port = process.env.PORT || 3000;
 app.use(cors())
 
 const categories = require('./data/Categories')
+const news = require('./data/news.json');
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Dragon Server is Running')
 })
 
-app.get('/categories', (req, res) =>{
+app.get('/categories', (req, res) => {
     // console.log(categories, 12)
     res.send(categories)
 })
 
-app.listen(port, () =>{
+// ---------------------------- News Related Apis -----------------
+
+// 1. All News
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+// 2. Specific News by id
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+// Category wise News 
+app.get('/categories/:id', (req, res) => {
+    // const id = req.params.id;
+    const id = parseInt(req.params.id);
+    // console.log(id);
+    // if (id === 0) {
+    if (id === 0) {
+        res.send(news)
+    } else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews);
+    }
+    // const categoryNews = news.filter(n => n.category_id === id);
+    // res.send(categoryNews);
+})
+
+app.listen(port, () => {
     console.log(`Dragon Server is Running on Port : ${port}`)
 })
